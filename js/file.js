7eaf34670098699456e842e764e98f4681f5bf84
Kausinho <kausinhobrunow@gmail.com>
2021-05-19 14:55:40 -0300

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,6 +1,11 @@
 $(function(){
-    $('nav a').hover(function(){
+    $('nav.desktop a').hover(function(){
         $('nav li').removeClass('selected');
         $(this).parent().addClass('selected');
     });
+
+    $('nav.mobile h3').click(function(){
+        $('ul.nav-desktop').slideToggle();
+    });
+    
 });
